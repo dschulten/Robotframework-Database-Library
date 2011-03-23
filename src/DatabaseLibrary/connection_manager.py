413731cb9c18b45180f30ef31beb5afa94d8acc3
Dietrich Schulten <ds@escalon.de>
2011-03-23 23:16:37 +0100

    added connect_to_dbapi with arbitrary connect arguments, this is needed for all dbapi modules which do not use the dbname, user, password scheme such as jaydebeapi or pymysql

--- src/DatabaseLibrary/connection_manager.py
+++ src/DatabaseLibrary/connection_manager.py
@@ -25,6 +25,32 @@ class ConnectionManager(object):
         """
         self._dbconnection = None
         
+
+    def connect_to_dbapi2(self, dbapiModuleName, *args):
+        """
+        Loads the DB API 2.0 module with the given `dbapiModuleName`, then uses 
+        it to connect to the database using `args`. The arguments depend on the 
+        dbapiModule which is used for the connection.
+        
+        Example usage:
+        | # Using psycopg2, specify postgres database name, user and password |
+        | Connect To Dbapi2 | psycopg2 | my_db | user | s3cr3t |
+        
+        | # Using pymysql, specify mysql database host, user, password, db and port |
+        | Connect To Dbapi2 | pymysql | localhost | user | s3cr3t | my_db | 3306 |
+
+        | # Using jaydebeapi, set classpath, connect via jdbc driver in Python or Jython |
+        | Set Environment Variable  CLASSPATH  ${CURDIR}/../libraries/ojdbc14-10.2.0.3.0.jar |
+        | Connect To Dbapi2 | jaydebeapi | oracle.jdbc.driver.OracleDriver | jdbc:oracle:thin:@host:port | user | s3cr3t |
+
+        """
+        db_api_2 = __import__(dbapiModuleName);
+        # TODO: pass configfile as second arg dbConfigFile="./resources/db.cfg"? 
+        # maybe use ConfigParser.SafeConfigParser({'bar': 'Life', 'baz': 'hard'})
+        # seems to be recommended by Python reference
+        # don't know how to handle config.items so that it can be passed as args
+        self._dbconnection = db_api_2.connect (*args);              
+        
     def connect_to_database(self, dbapiModuleName=None, dbName=None, dbUsername=None, dbPassword=None, dbConfigFile="./resources/db.cfg"):
         """
         Loads the DB API 2.0 module given `dbapiModuleName` then uses it to 
